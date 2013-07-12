40c77f00cf3645ea9795e70c212aae649392562b
Max <maksr51314@gmail.com>
2013-07-12 15:29:56 +0300

    Create first example

---
+++ js/gameOfLife.js
@@ -0,0 +1,103 @@
+var app = angular.module("gameOfLife", []);
+
+app.controller ('GameOfLifeCntl', function($scope){
+
+    //settings
+    $scope.height = $scope.width = 20;
+
+    //start Game
+    $scope.newGame = function () {
+        $scope.board = init($scope.height, $scope.width);
+    };
+    //build new board
+    function init(height, width) {
+        var board = [];
+        //h - height
+        for (var h = 0 ; h < height ; h++) {
+            var row = [];
+            //w- width
+            for (var w = 0 ; w < width ; w++) {
+                //add rows
+                row.push(false);
+            }
+            //add board
+            board.push(row);
+        }
+        return board;
+    }
+
+    //next step
+    $scope.next = function () {
+        $scope.board = computeNext($scope.board);
+    };
+    //create new figure
+    function computeNext(board) {
+        var newBoard = [];
+        //r -row
+        for (var r = 0 ; r < board.length ; r++) {
+            var newRow = [];
+            //c -cell
+            for (var c = 0 ; c < board[r].length ; c++) {
+                newRow.push(willLive(board, r, c) || newCell(board, r, c));
+            }
+            newBoard.push(newRow);
+        }
+        return newBoard;
+    }
+
+    //chick choose
+    $scope.toggle = function (row, cell) {
+        $scope.board[row][cell] = !$scope.board[row][cell];
+        console.log("You die or live new cell");
+    };
+
+
+    //game rules
+    //якщо у живої клітини два чи три сусіди – то вона лишається жити;
+    function willLive(board, row, cell) {
+        if (inBoard(board, row, cell)
+            && neighbours(board, row, cell) >= 2
+            && neighbours(board, row, cell) <= 3){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    //якщо у мертвої клітини рівно три сусіди – то вона оживає.
+    function newCell(board, row, cell) {
+        if (!inBoard(board, row, cell)
+            && neighbours(board, row, cell) == 3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //neighbours count
+    function neighbours(board, row, cell) {
+        var n = 0;
+        //button -> top ; left -> right without row+0 cell+0 (choose cell)
+        n += inBoard(board, row-1, cell-1) ? 1 : 0;
+        n += inBoard(board, row-1, cell+0) ? 1 : 0;
+        n += inBoard(board, row-1, cell+1) ? 1 : 0;
+        n += inBoard(board, row+0, cell-1) ? 1 : 0;
+        n += inBoard(board, row+0, cell+1) ? 1 : 0;
+        n += inBoard(board, row+1, cell-1) ? 1 : 0;
+        n += inBoard(board, row+1, cell+0) ? 1 : 0;
+        n += inBoard(board, row+1, cell+1) ? 1 : 0;
+        return n;
+    }
+
+    function inBoard(board, row, cell) {
+        if (row >= 0   && row < board.length &&
+            cell >= 0  && cell < board[row].length &&
+            board[row][cell])
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+});
---
+++ kk.js
@@ -0,0 +1,95 @@
+function GameOfLifeCntl($scope, $http, $resource) {
+
+    $scope.newGame = function () {
+        $scope.history = [];
+        $scope.board = init($scope.height, $scope.width);
+    };
+
+    $scope.next = function () {
+        $scope.history.push($scope.board);
+        $scope.board = computeNext($scope.board);
+    };
+
+    $scope.step = function (index) {
+        $scope.board = $scope.history[index];
+        $scope.history = $scope.history.slice(0, index);
+    };
+
+    $scope.toggle = function (row, cell) {
+        $scope.history = []; // Reset history as it is no longer accurate
+        $scope.board[row][cell] = !$scope.board[row][cell];
+    };
+
+    $scope.cellClass = function (row, cell) {
+        if (willDie($scope.board, row, cell)) {
+            return "die";
+        }
+        if (newCell($scope.board, row, cell)) {
+            return "new";
+        }
+        return "";
+    };
+
+    $scope.height = 10;
+    $scope.width = 20;
+    $scope.newGame();
+
+    function init(height, width) {
+        var board = [];
+        for (var h = 0 ; h < height ; h++) {
+            var row = [];
+            for (var w = 0 ; w < width ; w++) {
+                row.push(false);
+            }
+            board.push(row);
+        }
+        return board;
+    }
+
+    function computeNext(board) {
+        var newBoard = [];
+        for (var r = 0 ; r < board.length ; r++) {
+            var newRow = [];
+            for (var c = 0 ; c < board[r].length ; c++) {
+                newRow.push(willLive(board, r, c) || newCell(board, r, c));
+            }
+            newBoard.push(newRow);
+        }
+        return newBoard;
+    }
+
+    function willLive(board, row, cell) {
+        return cellAt(board, row, cell)
+            && neighbours(board, row, cell) >= 2
+            && neighbours(board, row, cell) <= 3;
+    }
+    function willDie(board, row, cell) {
+        return cellAt(board, row, cell)
+            && (neighbours(board, row, cell) < 2
+            || neighbours(board, row, cell) > 3);
+    }
+    function newCell(board, row, cell) {
+        return !cellAt(board, row, cell)
+            && neighbours(board, row, cell) == 3;
+    }
+
+    function neighbours(board, row, cell) {
+        var n = 0;
+        //button -> top ; left -> right without row+0 cell+0 (choose cell)
+        n += cellAt(board, row-1, cell-1) ? 1 : 0;
+        n += cellAt(board, row-1, cell+0) ? 1 : 0;
+        n += cellAt(board, row-1, cell+1) ? 1 : 0;
+        n += cellAt(board, row+0, cell-1) ? 1 : 0;
+        n += cellAt(board, row+0, cell+1) ? 1 : 0;
+        n += cellAt(board, row+1, cell-1) ? 1 : 0;
+        n += cellAt(board, row+1, cell+0) ? 1 : 0;
+        n += cellAt(board, row+1, cell+1) ? 1 : 0;
+        return n;
+    }
+
+    function cellAt(board, row, cell) {
+        return (row >= 0   && row < board.length &&
+            cell >= 0  && cell < board[row].length &&
+            board[row][cell]);
+    }
+}
